1f513abcdaf195b68ef79a693607f53b2c2a4700
abcdegen <141837183+abcdegen@users.noreply.github.com>
2026-05-17 23:18:57 +0300

    Delete IFileLifeController.cs

--- IFileLifeController.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab10;
-
-public interface IFileLifeController
-{
-    void CreateFile();
-    void DeleteFile();
-    void EditFile(string smth);
-    void ChangeFileExtension(string smth);
-}
